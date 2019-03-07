7e7574ae07cd389509a6f833d763a2e9182ab4b2
Rahul Makhija <rahuls360@gmail.com>
2019-03-07 19:06:04 +0530

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import data from "./api.json";
+
+//import bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+import Question from "./components/Question.js";
+if (typeof window !== "undefined") {
+  window.jQuery = window.$ = require("jquery");
+  require("bootstrap");
+}
 
 class App extends Component {
+  state = {
+    data: data.results
+  };
+
   render() {
     return (
-      <div className="App">
-        <h1>hi</h1>
+      <div className="container">
+        <div className="row">
+          <h1 className="text-center">Welcome to Trivia</h1>
+          <Question />
+        </div>
       </div>
     );
   }
