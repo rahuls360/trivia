458fdc1d275a534885cb73f601bfe4eb05828044
Rahul Makhija <rahuls360@gmail.com>
2019-03-07 19:41:34 +0530

    style the options element

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,14 +4,22 @@ class Question extends React.Component {
   render() {
     const options = this.props.incorrect_answers;
     options.push(this.props.correct_answer);
+
+    const optionLetters = ["A", "B", "C", "D"];
+
     return (
       <div className="question">
         <h3>
           Q{this.props.index + 1} {this.props.question}
         </h3>
         <ul>
-          {options.map(option => {
-            return <li>{option}</li>;
+          {options.map((option, index) => {
+            return (
+              <li className="option">
+                <span>{optionLetters[index]})</span>
+                {option}
+              </li>
+            );
           })}
         </ul>
       </div>
